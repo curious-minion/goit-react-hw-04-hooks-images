dfa31d27de82b19ed7017127932e5003f1b47944
courios-minion <okarskaolya@gmail.com>
2021-08-31 13:42:18 +0200

    updated with hooks

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,36 +1,33 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Searchbar, SearchForm, SearchForm_button, SearchForm_button_label, SearchForm_input} from './SearchBar.module.css';
 
-export default class SearchBar extends Component {
-  state = {
-    searchRequest: '',
+const SearchBar = ({onSubmit}) => {
+  const [searchRequest, setSearchRequest] = useState('');
+  
+  const handleRequestChange = event => {
+    setSearchRequest(event.currentTarget.value.toLowerCase());
   };
 
-  handleRequestChange = event => {
-    this.setState({ searchRequest: event.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
 
-    if (this.state.searchRequest.trim() === '') {
+    if (searchRequest.trim() === '') {
       toast.error('Type in your search request');
       return;
     }
     
-    this.props.onSubmit(this.state.searchRequest);
-    this.setState({ searchRequest: '' });
+   onSubmit(searchRequest);
+  setSearchRequest('');
 
   };
 
-  render() {
-    const { searchRequest } = this.state;
+  
     
     return (
       <header className={Searchbar}>
-      <form onSubmit={this.handleSubmit} className={SearchForm}>
+      <form onSubmit={handleSubmit} className={SearchForm}>
         <button type="submit" className={SearchForm_button}>
           <span className={SearchForm_button_label}>Search</span>
         </button>
@@ -40,14 +37,15 @@ export default class SearchBar extends Component {
             type="text"
             name="searchRequest"
             value={searchRequest}
-            onChange={this.handleRequestChange}
+            onChange={handleRequestChange}
           autoComplete="off"
           autoFocus
           placeholder="Search images and photos"
         />
       </form>
     </header>);
-  }
+  
 
 }
 
+export default SearchBar;
